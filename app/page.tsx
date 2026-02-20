f0ef9eafd504b1ba09abb1cb9df26b8043f1800e
DeepakENT <a5dgamingnetwork@gmail.com>
2026-02-20 19:39:48 +0530

    Add blue background div to main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <Navbar />
       <main className="bg-red-500 p-20">
         <h1 className="text-5xl text-white">
+          <div className="bg-blue-500 h-20"></div>
           Tailwind Working?
         </h1>
       </main>
