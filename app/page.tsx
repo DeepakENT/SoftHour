6f31e0ad5bf3dba2c84c802a946fc7503a51f4ab
DeepakENT <a5dgamingnetwork@gmail.com>
2026-02-20 18:56:14 +0530

    Refactor Home component to include Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <h1>Soft Hour Working</h1>;
+  return (
+    <>
+      <Navbar />
+      <main className="p-10">
+        <h1>Soft Hour Working</h1>
+      </main>
+    </>
+  );
 }
